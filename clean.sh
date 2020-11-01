08d148be3cdcdc6cc0448ed6ecd4af7afed94256
poifull10 <poifull10@gmail.com>
2020-11-01 15:50:35 +0900

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for dir in $(find gitlab -maxdepth 2 -mindepth 2)
+for dir in $(find gitlab -maxdepth 2 -mindepth 2 -not -name .gitkeep)
 do
   sudo rm -rf $dir
 done
 
-for dir in $(find gitlab-runner -maxdepth 2 -mindepth 2)
+for dir in $(find gitlab-runner -maxdepth 2 -mindepth 2 -not -name .gitkeep)
 do
   sudo rm -rf $dir
 done
